cc00912b2189a5ea1092d77a93a23906eb96872a
blesSmePadre <alan.hadzaragov@saltpepper.ru>
2019-10-21 13:54:32 +0300

    Fix scripts

--- tools/scripts/copy-favicon.js
+++ tools/scripts/copy-favicon.js
@@ -1,5 +1,8 @@
 const fs = require('fs-extra');
+const fsExtra = require('fs-extra');
 const path = require('path');
+
+fsExtra.ensureDirSync('public');
 
 fs.copySync(
   path.resolve(process.cwd(), 'src/assets/favicons'),
--- tools/scripts/mock-manifest.js
+++ tools/scripts/mock-manifest.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
+const fsExtra = require('fs-extra');
 const path = require('path');
+
+fsExtra.ensureDirSync('public');
 
 fs.writeFileSync(
   path.resolve(process.cwd(), 'public/webpack-assets.json'),
