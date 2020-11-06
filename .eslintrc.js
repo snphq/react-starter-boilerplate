246069a28903742ab13a84ad652669ef25728633
Mikhail Kaminsky <kam.m.a@ya.ru>
2020-11-06 11:47:37 +0300

    Add eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,6 +54,7 @@ module.exports = {
 
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'func-names': ["error", "never"],
   },
   globals: {
     RUNTIME_ENV: true,
